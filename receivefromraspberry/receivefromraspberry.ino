ba02e06fcd79d0501b073d6ff2d73661b3e97028
StephanEnseleit <stephan.enseleit@gmail.com>
2021-10-01 17:36:21 +0200

    rotary encoder test

--- receivefromraspberry/receivefromraspberry.ino
+++ receivefromraspberry/receivefromraspberry.ino
@@ -1,37 +1,73 @@
 #include <Wire.h>
 #include <string.h>
 #include <AccelStepper.h>
-#include <MultiStepper.h>
+#include <EnableInterrupt.h> 
+
+#define encoderA 5
+#define encoderB 6
+#define encoderZ 7
 
 byte slave_address = 7;
 long positions[2];
 int counter = 0;
+
+volatile int countA = 0;
+volatile int countB = 0;
+volatile int cumulativeCountA = 0;
+volatile int cumulativeCountB = 0;
+int pulsesPerRev = 1000;     //This variable will is unique to the type of encoder
+int Dir = 0;  // 1 = CW
+              // 0 = Stationary
+              // -1 = CCW
+              
 // Pulses D3, direction D4, Enable D5
-AccelStepper stepper1(1,3, 4); //degree motor
-//AccelStepper stepper2(AccelStepper::FULL4WIRE, 8, 9, 10, 11); //radius motor
+AccelStepper rotor(1, 3, 4); //degree motor
+AccelStepper radius(1, 5, 6); //radius motor
 
 void setup() {
   Wire.begin(slave_address);
   Wire.onReceive(receiveEvent);
   Serial.begin(9600);  
 
-  stepper1.setMaxSpeed(30000);
+  rotor.setMaxSpeed(30000);
  // stepper2.setMaxSpeed(100);
-  stepper1.setAcceleration(1500);
+  rotor.setAcceleration(1500);
  // stepper2.setAcceleration(20);
+
+  pinMode(encoderA, INPUT_PULLUP);
+  pinMode(encoderB, INPUT_PULLUP);
+  pinMode(encoderZ, INPUT_PULLUP);  
+  enableInterrupt(encoderA, pulseA, RISING);
+  enableInterrupt(encoderB, pulseB, RISING);
+  enableInterrupt(encoderZ, pulseZ, RISING);
 }
 
 void loop() {
-  //if (counter % 2 == 0) {
-      //probably needs to add some factor, maybe steps for one whole rotation
-      
-      stepper1.moveTo(positions[0]); //set distance
-      while (stepper1.distanceToGo() > 0) {
-          stepper1.run();
-      }
-   // }
+  Serial.print(countA);
+  Serial.print('\t');
+  Serial.print(countB);
+  Serial.print('\t');
+  Serial.print(cumulativeCountA);
+  Serial.print('\t');
+  Serial.print(cumulativeCountB);
+  Serial.print('\t');
+  Serial.println(Dir); 
 }
 
+
+/**
+ * moves bot in angle direction
+ */
+void moveRotor(int angle) {
+  rotor.moveTo(angle); //set distance
+      while (rotor.distanceToGo() > 0) {
+          rotor.run();
+      }
+  }
+
+/**
+ * I2C Receive and make integer values
+ */
 void receiveEvent(int howMany) {
   counter += 1;
   int numOfBytes = Wire.available();
@@ -50,4 +86,34 @@ void receiveEvent(int howMany) {
     positions[1] = atoi(str);
     }
   Serial.println(positions[0]);
+  moveRotor(positions[0]);
+}
+
+/*
+ * ***** Rotary Encoder Functions *****
+ */
+
+void checkDirection(){
+  if(digitalRead(encoderB) ==  HIGH){                             //digitalReadFast() is faster than digitalRead()
+    Dir = 1;  
+  }
+  else if(digitalRead(encoderB) ==  LOW){
+    Dir = -1;
+  }
+}
+
+void pulseA(){  
+  checkDirection();
+  countA += Dir;
+  cumulativeCountA += Dir;
+}
+
+void pulseB(){  
+  countB += Dir;
+  cumulativeCountB += Dir;  
+}
+
+void pulseZ(){
+  countA = 0;   //reset counters at "home" reference point
+  countB = 0;
 }
